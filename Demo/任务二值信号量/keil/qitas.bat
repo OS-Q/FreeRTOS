eb4b9de69ec7a37e5bcc3e0cf92214f54509c164
qitas <qitas@hotmail.com>
2019-06-03 22:04:38 +0800

    fix keil output

--- Demo/任务二值信号量/keil/qitas.bat
+++ Demo/任务二值信号量/keil/qitas.bat
@@ -1,9 +1,10 @@
-del *.lst /s
 del *.bin /s
 del *.uvoptx /s
 del *.uvguix.* /s
-rd Obj /s /q
-rd List /s /q
+del *.uvopt /s
+del *.uvgui.* /s
+rd obj /s /q
+rd list /s /q
 rd RTE /s /q
 rd DebugConfig /s /q
 exit
