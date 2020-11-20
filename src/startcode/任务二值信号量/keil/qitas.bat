eeeb33cae7e275c7b8e3b5b4acefbac5850b467e
qitas <qitas@qitas.cn>
2020-11-20 17:21:28 +0800

    del

--- src/startcode/任务二值信号量/keil/qitas.bat
+++ src/startcode/任务二值信号量/keil/qitas.bat
@@ -1,10 +1,15 @@
-del *.bin /s
-del *.uvoptx /s
-del *.uvguix.* /s
-del *.uvopt /s
-del *.uvgui.* /s
+del *.uvop* /s
+del *.uvgui* /s
+del *.saved* /s
+del *.o /s
+del *.d /s
+del *.crf /s
+del *.htm /s
+del *.lnp /s
+del *.sct /s
+del *.axf /s
 rd obj /s /q
 rd list /s /q
-rd RTE /s /q
 rd DebugConfig /s /q
+rd RTE /s /q
 exit
