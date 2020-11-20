c2aca057987ee4e5c0e71c1c73defcae1812b0cd
qitas <qitas@qitas.cn>
2020-11-20 08:51:14 +0800

    fix

--- src/startcode/单消息队列覆盖/User/main.c
+++ src/startcode/单消息队列覆盖/User/main.c
@@ -1,45 +1,4 @@
-/*
-*********************************************************************************************************
-*
-*	模块名称 : 主程序模块。
-*	文件名称 : main.c
-*	版    本 : V1.0
-*	说    明 : 本实验主要学习FreeRTOS的单队列消息覆盖
-*              实验目的：
-*                1. 学习FreeRTOS的单队列消息覆盖
-*                   单消息队列是指消息队列的长度是1，覆盖方式是指消息队列中已经由数据了，还可以向
-*                   消息队列中发数据，覆盖消息队列中已有的数据。
-*                2. 本实验使用的函数是xQueueOverwrite，此函数仅适用于消息队列长度为1的情况。
-*                   否则将导致触发configASSERT()，从而进入假死状态（如果宏定义使能了configASSERT）。
-*              实验内容：
-*                3. 按下按键K1可以通过串口打印任务执行情况
-*                   任务名      任务状态 优先级   剩余栈 任务序号
-*                   vTaskUserIF     R       1       332     1
-*                   IDLE            R       0       120     5
-*                   vTaskLED        B       2       478     2
-*                   vTaskMsgPro     B       3       478     3
-*                   vTaskStart      B       4       490     4
-*
-*                   任务名       运行计数         使用率
-*                   vTaskUserIF     2299            2%
-*                   IDLE            76868           96%
-*                   vTaskLED        19              <1%
-*                   vTaskMsgPro     14              <1%
-*                   vTaskStart      1               <1%
-*                   串口软件建议使用SecureCRT（V4光盘里面有此软件）查看打印信息。
-*                    vTaskTaskUserIF 任务：按键消息处理
-*                    vTaskLED 任务       ：使用函数xQueueReceive接收任务vTaskTaskUserIF发送的消息队列数据(xQueue2)
-*                    vTaskMsgPro 任务    ：使用函数xQueueReceive接收任务vTaskTaskUserIF发送的消息队列数据(xQueue1)
-*                    vTaskStart 任务     ：按键扫描
-*                 4. 任务运行转态的定义如下，跟上面串口打印字母B, R, D, S对应：
-*                    #define tskBLOCKED_CHAR		( 'B' )
-*                    #define tskREADY_CHAR		    ( 'R' )
-*                    #define tskDELETED_CHAR		( 'D' )
-*                    #define tskSUSPENDED_CHAR	    ( 'S' )
-*                 5. K2键按下，向xQueue1发送数据。
-*                 6. K3键按下，向xQueue2发送数据。
-*********************************************************************************************************
-*/
+
 #include "includes.h"
 
 
@@ -84,17 +43,17 @@ MSG_T   g_tMsg; /* 定义一个结构体用于消息队列 */
 int main(void)
 {
 	/* 硬件初始化初始化 */
-	bsp_Init(); 
-	
+	bsp_Init();
+
 	/* 初始化一个定时器中断，精度高于滴答定时器中断，这样才可以获得准确的系统信息 */
 	vSetupSysInfoTest();
-	
+
 	/* 创建任务 */
 	AppTaskCreate();
-	
+
 	/* 创建任务通信机制 */
 	AppObjCreate();
-	
+
     /* 启动调度，开始执行任务 */
     vTaskStartScheduler();
 
@@ -105,7 +64,7 @@ int main(void)
 /*
 *********************************************************************************************************
 *	函 数 名: vTaskTaskUserIF
-*	功能说明: 按键消息处理		
+*	功能说明: 按键消息处理
 *	形    参: pvParameters 是在创建该任务时传递的形参
 *	返 回 值: 无
 *   优 先 级: 1  (数值越小优先级越低，这个跟uCOS相反)
@@ -117,10 +76,10 @@ static void vTaskTaskUserIF(void *pvParameters)
 	uint8_t ucCount = 0;
 	uint8_t ucKeyCode;
 	uint8_t pcWriteBuffer[500];
-	
+
 	/* 初始化结构体指针 */
 	ptMsg = &g_tMsg;
-	
+
 	/* 初始化数组 */
 	ptMsg->ucMessageID = 0;
 	ptMsg->ulData[0] = 0;
@@ -129,7 +88,7 @@ static void vTaskTaskUserIF(void *pvParameters)
     while(1)
     {
 		ucKeyCode = bsp_GetKey();
-		
+
 		if (ucKeyCode != KEY_NONE)
 		{
 			switch (ucKeyCode)
@@ -140,12 +99,12 @@ static void vTaskTaskUserIF(void *pvParameters)
 					printf("任务名      任务状态 优先级   剩余栈 任务序号\r\n");
 					vTaskList((char *)&pcWriteBuffer);
 					printf("%s\r\n", pcWriteBuffer);
-				
+
 					printf("\r\n任务名       运行计数         使用率\r\n");
 					vTaskGetRunTimeStats((char *)&pcWriteBuffer);
 					printf("%s\r\n", pcWriteBuffer);
 					break;
-				
+
 				/* K2键按下，向xQueue1发送数据 */
 				case KEY_DOWN_K2:
 					ucCount++;
@@ -153,23 +112,23 @@ static void vTaskTaskUserIF(void *pvParameters)
 					xQueueOverwrite(xQueue1, (void *) &ucCount);
 					printf("K2键按下，向xQueue1发送数据，即使消息队列已经满了\r\n");
 					break;
-				
+
 				/* K3键按下，向xQueue2发送数据 */
 				case KEY_DOWN_K3:
 					ptMsg->ucMessageID++;
 					ptMsg->ulData[0]++;;
 					ptMsg->usData[0]++;
-					
+
 					/* 使用消息队列实现指针变量的传递 */
 					xQueueOverwrite(xQueue2, (void *) &ptMsg);
 					printf("K3键按下，向xQueue2发送数据，即使消息队列已经满了\r\n");
-				
+
 				/* 其他的键值不处理 */
-				default:                     
+				default:
 					break;
 			}
 		}
-		
+
 		vTaskDelay(10);
 	}
 }
@@ -180,7 +139,7 @@ static void vTaskTaskUserIF(void *pvParameters)
 *	功能说明: 使用函数xQueueReceive接收任务vTaskTaskUserIF发送的消息队列数据
 *	形    参: pvParameters 是在创建该任务时传递的形参
 *	返 回 值: 无
-*   优 先 级: 2  
+*   优 先 级: 2
 *********************************************************************************************************
 */
 static void vTaskLED(void *pvParameters)
@@ -188,14 +147,14 @@ static void vTaskLED(void *pvParameters)
 	MSG_T *ptMsg;
 	BaseType_t xResult;
 	const TickType_t xMaxBlockTime = pdMS_TO_TICKS(200); /* 设置最大等待时间为200ms */
-	
+
     while(1)
     {
 		xResult = xQueueReceive(xQueue2,                   /* 消息队列句柄 */
 		                        (void *)&ptMsg,  		   /* 这里获取的是结构体的地址 */
 		                        (TickType_t)xMaxBlockTime);/* 设置阻塞时间 */
-		
-		
+
+
 		if(xResult == pdPASS)
 		{
 			/* 成功接收，并通过串口将数据打印出来 */
@@ -218,7 +177,7 @@ static void vTaskLED(void *pvParameters)
 *	功能说明: 使用函数xQueueReceive接收任务vTaskTaskUserIF发送的消息队列数据
 *	形    参: pvParameters 是在创建该任务时传递的形参
 *	返 回 值: 无
-*   优 先 级: 3  
+*   优 先 级: 3
 *********************************************************************************************************
 */
 static void vTaskMsgPro(void *pvParameters)
@@ -226,13 +185,13 @@ static void vTaskMsgPro(void *pvParameters)
 	BaseType_t xResult;
 	const TickType_t xMaxBlockTime = pdMS_TO_TICKS(300); /* 设置最大等待时间为300ms */
 	uint8_t ucQueueMsgValue;
-	
+
     while(1)
     {
 		xResult = xQueueReceive(xQueue1,                   /* 消息队列句柄 */
 		                        (void *)&ucQueueMsgValue,  /* 存储接收到的数据到变量ucQueueMsgValue中 */
 		                        (TickType_t)xMaxBlockTime);/* 设置阻塞时间 */
-		
+
 		if(xResult == pdPASS)
 		{
 			/* 成功接收，并通过串口将数据打印出来 */
@@ -253,7 +212,7 @@ static void vTaskMsgPro(void *pvParameters)
 *	功能说明: 启动任务，也就是最高优先级任务。
 *	形    参: pvParameters 是在创建该任务时传递的形参
 *	返 回 值: 无
-*   优 先 级: 4  
+*   优 先 级: 4
 *********************************************************************************************************
 */
 static void vTaskStart(void *pvParameters)
@@ -282,23 +241,23 @@ static void AppTaskCreate (void)
                     NULL,              /* 任务参数  */
                     1,                 /* 任务优先级*/
                     NULL );            /* 任务句柄  */
-	
-	
+
+
 	xTaskCreate(    vTaskLED,        	/* 任务函数  */
                     "vTaskLED",      	/* 任务名    */
                     512,             	/* stack大小，单位word，也就是4字节 */
                     NULL,            	/* 任务参数  */
                     2,                  /* 任务优先级*/
                     &xHandleTaskLED );  /* 任务句柄  */
-	
+
 	xTaskCreate(    vTaskMsgPro,     /* 任务函数  */
                     "vTaskMsgPro",   /* 任务名    */
                     512,             /* stack大小，单位word，也就是4字节 */
                     NULL,            /* 任务参数  */
                     3,               /* 任务优先级*/
                     &xHandleTaskMsgPro );  /* 任务句柄  */
-	
-	
+
+
 	xTaskCreate(    vTaskStart,     /* 任务函数  */
                     "vTaskStart",   /* 任务名    */
                     512,            /* stack大小，单位word，也就是4字节 */
@@ -307,14 +266,12 @@ static void AppTaskCreate (void)
                     NULL );         /* 任务句柄  */
 }
 
-/*
-*********************************************************************************************************
-*	函 数 名: AppObjCreate
-*	功能说明: 创建任务通信机制
-*	形    参: 无
-*	返 回 值: 无
-*********************************************************************************************************
-*/
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+********************************************************************************/
 static void AppObjCreate (void)
 {
 	/* 创建1个uint8_t型消息队列 */
@@ -323,7 +280,7 @@ static void AppObjCreate (void)
     {
         /* 没有创建成功，用户可以在这里加入创建失败的处理机制 */
     }
-	
+
 	/* 创建1个存储指针变量的消息队列，由于CM3是32位机，一个指针变量占用4个字节 */
 	xQueue2 = xQueueCreate(1, sizeof(struct Msg *));
     if( xQueue2 == 0 )
@@ -332,3 +289,4 @@ static void AppObjCreate (void)
     }
 }
 
+/*----------------------- (C) COPYRIGHT 2020 www.OS-Q.comm --------------------*/
